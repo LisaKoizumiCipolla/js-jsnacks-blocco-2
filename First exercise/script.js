be792be5ed1cca5404a1bc017d44d398f53fc00f
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-05-19 16:54:38 +0200

    First excercise

---
+++ First exercise/script.js
@@ -0,0 +1,19 @@
+let userNumbers = [];
+
+let sum = 0;
+
+while ( sum < 50 ){
+    let i = parseInt(prompt("Insert number"));
+    userNumbers.push(i);
+    sum += i;
+    
+}
+
+
+console.log(sum)
+
+/* Inserire il numero
+Aggiungerlo all'array
+Addizinarlo agli altri numeri nell'array
+Se il risultato è minore di 50, interrompere
+*/
