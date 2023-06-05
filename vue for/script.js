c67e0787eb847a72283d76626958cba9d24af614
LisaKoizumiCipolla <cipollalisa17@gmail.com>
2023-06-05 12:42:49 +0200

    Snack

---
+++ vue for/script.js
@@ -0,0 +1,9 @@
+const {createApp} = Vue;
+createApp({
+    data(){
+        return{
+            listFruits : ["granadilla", "dragonfruit", "orange", "ranbutan", "lychees", "mangosteen"],
+        }
+    }
+
+}).mount("#vue")
